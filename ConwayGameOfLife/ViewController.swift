1bbb16235710ffba5d366973c0f19f9a74c5211c
Jun Dang <jun.dang8@gmail.com>
2019-11-22 11:00:28 -0500

    Initial Commit

---
+++ ConwayGameOfLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConwayGameOfLife
+//
+//  Created by Jun Dang on 11/22/19.
+//  Copyright © 2019 Jun Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
